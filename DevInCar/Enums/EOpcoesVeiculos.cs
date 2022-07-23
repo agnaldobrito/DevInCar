f72ba95722ce6c370efea0a31c5266a63c3f2f81
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-07-23 11:41:48 -0300

    add: tipos de veiculos

---
+++ DevInCar/Enums/EOpcoesVeiculos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevInCar.Enums
+{
+    public enum EOpcoesVeiculos
+    {
+        
+        MotosOuTriciclos = 1,
+        Carros,
+        Caminhonetes,
+        Todos,
+
+    }
+}
