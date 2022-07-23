d2d2543b205547d7663c31c130692c0c5107e5f3
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-07-23 11:44:03 -0300

    refactor: refatorando as listas

---
+++ DevInCar/Dapper/Data.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevInCar.Models;
+using DevInCar.Enums;
+
+
+namespace DevInCar.Dapper
+{
+    public static class Data
+    {
+        public static List<Veiculo> veiculos = new()
+        {
+                new Carro ( totalPortas: 4, tipoCombustivel: ECombustiveis.Flex, nome: "Toyota - Etios",
+                valor: 69999, cor: ECores.Preto, potencia: 92, veiculoVendido: true ),
+                new Carro ( 2, ECombustiveis.Gasolina, "Mazda - RX7", 189999, ECores.Vermelho, 143, true ),
+                new Carro ( 2, ECombustiveis.Gasolina, "Nissan - NSX 2000", 210000, ECores.Azul, 273, false ),
+                new Carro ( 4, ECombustiveis.Gasolina, "Mitsubishi - Lancer EVO V", 215999, ECores.Branco, 298, false ),
+
+                new MotoTriciclo( totalRodas: 2, nome: "Suzuki - Hayabusa", valor: 120000, cor:ECores.Amarelo, potencia:175, false ),
+                new MotoTriciclo( 3, "Tuk-tuk", 23500, ECores.Laranja, 107, true),
+                new MotoTriciclo( 2, "Suzuki - GSX-R1000R", 60500, ECores.Branco, 107, true),
+                new MotoTriciclo( 2, "Honda - CG 160", 45500, ECores.Cinza, 107, false),
+
+                new Caminhonete( capacidadeCarga: 220.5m, totalPortas: 4,ECombustiveis.Diesel, nome: "Toyota - Hylux",
+                                 valor:189999, ECores.Roxo, potencia: 277, veiculoVendido: true ),
+                new Caminhonete(215, 2, ECombustiveis.Diesel, "Mitsubishi - L200", 130500, ECores.Roxo, 239, false)
+        };
+        
+    }
+}
---
+++ DevInCar/Dapper/DataFilter.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevInCar.Models;
+using DevInCar.Enums;
+
+namespace DevInCar.Dapper
+{
+    public class DataFilter
+    {
+        public static void FiltrarMotos(int opcao)
+        {
+            
+
+            if (opcao == 1)
+            {
+                var motosOuTriciclos =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.MotosOuTriciclos)
+                    && veiculo.VeiculoVendido != true);
+
+                foreach (var motoOuTricilo in motosOuTriciclos)
+                {
+                    Console.WriteLine(motoOuTricilo.ListarInfo());
+                }
+            }
+            if (opcao == 2)
+            {
+                var motosOuTriciclos =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.MotosOuTriciclos)
+                    && veiculo.VeiculoVendido == true);
+
+                foreach (var motoOuTricilo in motosOuTriciclos)
+                {
+                    Console.WriteLine(motoOuTricilo.ListarInfo());
+                }
+            }
+            if (opcao == 3)
+            {
+                var motosOuTriciclos =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.MotosOuTriciclos)
+                    && veiculo.VeiculoVendido == true);
+
+                var maiorPreco = motosOuTriciclos.OrderByDescending(maisCaro => maisCaro.Valor).First();
+
+                Console.WriteLine(maiorPreco.ListarInfo());
+            }
+            if (opcao == 4)
+            {
+                var motosOuTriciclos =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.MotosOuTriciclos)
+                    && veiculo.VeiculoVendido == true);
+
+                var menorPreco = motosOuTriciclos.OrderBy(maisBarato => maisBarato.Valor).First();
+
+                Console.WriteLine(menorPreco.ListarInfo());
+            }
+        }
+        public static void FiltrarCarros(int opcao)
+        {
+
+            if (opcao == 1)
+            {
+                var carros =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.Carros)
+                    && veiculo.VeiculoVendido != true);
+
+                foreach (var carro in carros)
+                {
+                    Console.WriteLine(carro.ListarInfo());
+                }
+            }
+            if (opcao == 2)
+            {
+                var carros =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.Carros)
+                    && veiculo.VeiculoVendido == true);
+
+                foreach (var carro in carros)
+                {
+                    Console.WriteLine(carro.ListarInfo());
+                }
+            }
+            if (opcao == 3)
+            {
+                var carros =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.Carros)
+                    && veiculo.VeiculoVendido == true);
+
+                var maiorPreco = carros.OrderBy(maisCaro => maisCaro.Valor).First();
+
+                Console.WriteLine(maiorPreco.ListarInfo());
+
+
+            }
+            if (opcao == 4)
+            {
+                var carros =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.Carros)
+                    && veiculo.VeiculoVendido == true);
+
+                var menorPreco = carros.OrderByDescending(maisBarato => maisBarato.Valor).First();
+
+                Console.WriteLine(menorPreco.ListarInfo());
+
+
+            }
+        }
+        public static void FiltrarCaminhonetes(int opcao)
+        {
+
+            if (opcao == 1)
+            {
+                var caminhonetes =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.Caminhonetes)
+                    && veiculo.VeiculoVendido != true);
+
+                foreach (var caminhonete in caminhonetes)
+                {
+                    Console.WriteLine(caminhonete.ListarInfo());
+                }
+            }
+            if (opcao == 2)
+            {
+                var caminhonetes =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.Caminhonetes)
+                    && veiculo.VeiculoVendido == true);
+
+                foreach (var caminhonete in caminhonetes)
+                {
+                    Console.WriteLine(caminhonete.ListarInfo());
+                }
+            }
+            if (opcao == 3)
+            {
+                var caminhonetes =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.Caminhonetes)
+                    && veiculo.VeiculoVendido == true);
+
+                var maiorPreco = caminhonetes.OrderBy(maisCaro => maisCaro.Valor).First();
+
+                Console.WriteLine(maiorPreco.ListarInfo());
+
+
+            }
+            if (opcao == 4)
+            {
+                var caminhonetes =
+                Data.veiculos.Where(
+                    veiculo =>
+                    veiculo.TipoVeiculo.Equals(EOpcoesVeiculos.Carros)
+                    && veiculo.VeiculoVendido == true);
+
+                var menorPreco = caminhonetes.OrderByDescending(maisBarato => maisBarato.Valor).First();
+
+                Console.WriteLine(menorPreco.ListarInfo());
+
+
+            }
+        }
+    }
+}
